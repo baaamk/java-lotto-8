0c5dd37941928372aecfb7b73a85a92c80b5fd58
baaamk <brandon55321@gmail.com>
2025-11-01 04:49:00 +0900

    test: 로또 번호 검증 기능 테스트

--- src/test/java/lotto/model/domain/vo/LottoNumberTest.java
+++ src/test/java/lotto/model/domain/vo/LottoNumberTest.java
@@ -1,9 +1,13 @@
 package lotto.model.domain.vo;
 
+import lotto.exception.ErrorMessage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LottoNumberTest {
 
@@ -13,5 +17,15 @@ class LottoNumberTest {
     void _1_에서_45_이내의_숫자가_들어왔을_경우_정상_반환_한다(int parsedLottoNumber) {
         LottoNumber lottoNumber = LottoNumber.from(parsedLottoNumber);
         Assertions.assertEquals(LottoNumber.from(parsedLottoNumber), lottoNumber);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, -1})
+    @DisplayName("1~45 이외의 숫자가 들어왔을 경우 예외 처리 한다.")
+    void _1_에서_45_이외의_숫자가_들어왔을_경우_예외_처리_한다(int parsedLottoNumber) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> LottoNumber.from(parsedLottoNumber))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessage.INVALID_NUMBER_RANGE.getMessage()));
     }
 }
