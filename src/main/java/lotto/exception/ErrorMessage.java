ff64361c06e3b361cd6f97c3034a86db30efc591
baaamk <brandon55321@gmail.com>
2025-11-01 17:42:14 +0900

    feat(exception): 로또 번호 예외 구현

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     DUPLICATE_BONUS_NUMBER("당첨번호와 중복된 숫자를 입력하지 마세요."),
     INVALID_MONEY_MIN("최소 입력 금액은 1,000원 입니다."),
     INVALID_MONEY_MAX("1회 최대 입력 금액은 100,000원 입니다."),
-    INVALID_MONEY_UNIT("1,000원 단위로 입력해 주세요.");
+    INVALID_MONEY_UNIT("1,000원 단위로 입력해 주세요."),
+    INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("중복된 당첨번호가 있습니다.");
 
     private final String message;
 
