3b396808de3e3a904cde0583929a7666a8f6a099
baaamk <brandon55321@gmail.com>
2025-11-01 15:39:32 +0900

    test: 보너스 숫자 중복 예외 테스트 구현

--- src/test/java/lotto/model/domain/vo/BonusNumberTest.java
+++ src/test/java/lotto/model/domain/vo/BonusNumberTest.java
@@ -1,11 +1,13 @@
 package lotto.model.domain.vo;
 
-import org.junit.jupiter.api.Assertions;
+import lotto.exception.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusNumberTest {
@@ -19,4 +21,13 @@ class BonusNumberTest {
         assertTrue(bonusNumber.isMatchedWith(LottoNumber.from(number)));
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, -1})
+    @DisplayName("1~45 이외의 숫자가 들어왔을 경우 예외 처리 한다.")
+    void _1_에서_45_이외의_숫자가_들어왔을_경우_예외_처리_한다(int parsedLottoNumber) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> BonusNumber.from(parsedLottoNumber))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ErrorMessage.INVALID_NUMBER_RANGE.getMessage()));
+    }
 }
