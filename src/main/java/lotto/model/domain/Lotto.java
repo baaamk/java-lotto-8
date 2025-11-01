c260043213faf67b2c14ba6427dea4b06831ba2a
baaamk <brandon55321@gmail.com>
2025-11-01 17:41:24 +0900

    refactor(domain): 로또 도메인 생성 및 검증 기능 구현

--- src/main/java/lotto/model/domain/Lotto.java
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -1,20 +1,37 @@
 package lotto.model.domain;
 
+import lotto.exception.ErrorMessage;
+import lotto.model.domain.vo.LottoNumber;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    private final List<LottoNumber> lottoNumbers;
+
+    private Lotto(List<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    public static Lotto from(List<Integer> lottoNumbers) {
+        return new Lotto(
+                lottoNumbers.stream().map(LottoNumber::from).toList(),
+        );
+    }
+
+    private void validate(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
+        }
+
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<LottoNumber> getLottoNumbers() {
+        return lottoNumbers;
+    }
 }
