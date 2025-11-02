c26f36a9c04b6dd4cdea3aafb53fcf68e38d8ddc
baaamk <brandon55321@gmail.com>
2025-11-03 03:25:24 +0900

    feat(domain): 생성된 로또 일급컬렉션 기능 구현

---
+++ src/main/java/lotto/model/domain/Lottos.java
@@ -0,0 +1,22 @@
+package lotto.model.domain;
+
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos of(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+
+    public List<Lotto> values() {
+        return List.copyOf(lottos);
+    }
+
+}
