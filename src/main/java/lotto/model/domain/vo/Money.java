56d3bad61ba50d185f5cfa25d91d50b972ff341e
baaamk <brandon55321@gmail.com>
2025-11-03 03:31:59 +0900

    refactor(domain): 돈 검증 및 개수 반환 기능 구현

--- src/main/java/lotto/model/domain/vo/Money.java
+++ src/main/java/lotto/model/domain/vo/Money.java
@@ -2,16 +2,24 @@ package lotto.model.domain.vo;
 
 import lotto.exception.ErrorMessage;
 
-public class InputMoney {
+public class Money {
+    private static final int THOUSAND_UNIT = 1_000;
+    private static final int ZERO = 0;
+    private static final int MAX_MONEY_INPUT_AMOUNT = 100_000;
+
     private final int inputMoney;
 
-    public InputMoney(int inputMoney) {
+    public Money(int inputMoney) {
         validate(inputMoney);
         this.inputMoney = inputMoney;
     }
 
-    public static InputMoney from(int inputMoney) {
-        return new InputMoney(inputMoney);
+    public static Money from(int inputMoney) {
+        return new Money(inputMoney);
+    }
+
+    public int returnAmount() {
+        return inputMoney / THOUSAND_UNIT;
     }
 
     private void validate(int inputMoney) {
@@ -20,16 +28,16 @@ public class InputMoney {
     }
 
     private static void validateUnitOfMoney(int inputMoney) {
-        if (inputMoney % 1_000 != 0) {
+        if (inputMoney % THOUSAND_UNIT != ZERO) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_UNIT.getMessage());
         }
     }
 
     private static void validateMoneyRange(int inputMoney) {
-        if (inputMoney < 1_000) {
+        if (inputMoney < THOUSAND_UNIT) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_MIN.getMessage());
         }
-        if (inputMoney > 100_000) {
+        if (inputMoney > MAX_MONEY_INPUT_AMOUNT) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_MAX.getMessage());
         }
     }
