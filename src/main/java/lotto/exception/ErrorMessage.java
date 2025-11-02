225e5f1a7b68d0e414e5337b4c2e803e7d16d371
baaamk <brandon55321@gmail.com>
2025-11-03 03:37:32 +0900

    refactor(exception): 에러 및 perfix 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -7,17 +7,19 @@ public enum ErrorMessage {
     INVALID_MONEY_MAX("1회 최대 입력 금액은 100,000원 입니다."),
     INVALID_MONEY_UNIT("1,000원 단위로 입력해 주세요."),
     INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다."),
-    DUPLICATE_LOTTO_NUMBER("중복된 당첨번호가 있습니다.");
+    DUPLICATE_LOTTO_NUMBER("중복된 당첨번호가 있습니다."),
+    INVALID_NUMBER_INPUT("0 이상의 숫자만 입력해 주세요."),
+    INVALID_WINNING_LOTTO_INPUT("쉼표로 구분된 6개의 숫자를 입력해 주세요.");
 
     private final String message;
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
 
     ErrorMessage(String message) {
-        this.message = message;
+        this.message = ERROR_MESSAGE + message;
     }
 
     public String getMessage() {
         return message;
     }
-
-
 }
