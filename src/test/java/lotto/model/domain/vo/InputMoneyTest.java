a660e910cbb028da66d0e470dc8379ac49c78ed3
baaamk <brandon55321@gmail.com>
2025-11-03 03:39:11 +0900

    test: 테스트 클래스 변경

--- src/test/java/lotto/model/domain/vo/InputMoneyTest.java
+++ src/test/java/lotto/model/domain/vo/InputMoneyTest.java
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-class InputMoneyTest {
+class MoneyTest {
 
     @Test
     @DisplayName("1000원 단위의 숫자가 들어올 경우 정상 반환한다.")
     void _1000원_단위의_숫자가_들어올_경우_정상_반환한다() {
-        InputMoney inputMoney = InputMoney.from(1_000);
+        Money inputMoney = Money.from(1_000);
         Assertions.assertEquals(1_000, inputMoney.getInputMoney());
     }
 
     @Test
     @DisplayName("1000원 미만의 숫자가 들어올 경우 예외처리")
     void _1000_원_미만의_숫자가_들어올_경우_예외처리() {
-        assertThatThrownBy(() -> InputMoney.from(900))
+        assertThatThrownBy(() -> Money.from(900))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_MONEY_MIN.getMessage());
     }
@@ -27,7 +27,7 @@ class InputMoneyTest {
     @Test
     @DisplayName("100,000원 초과의 숫자가 들어올 경우 예외처리")
     void _100_000_원_초과의_숫자가_들어올_경우_예외처리() {
-        assertThatThrownBy(() -> InputMoney.from(101_300))
+        assertThatThrownBy(() -> Money.from(101_300))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_MONEY_MAX.getMessage());
     }
@@ -35,7 +35,7 @@ class InputMoneyTest {
     @Test
     @DisplayName("1,000원 단위가 아닌 숫자가 들어올 경우 예외처리")
     void _1_000_원_단위가_아닌_숫자가_들어올_경우_예외처리() {
-        assertThatThrownBy(() -> InputMoney.from(9_500))
+        assertThatThrownBy(() -> Money.from(9_500))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_MONEY_UNIT.getMessage());
     }
