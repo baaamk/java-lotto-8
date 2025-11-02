9f1e39825a6ae5cc92e2a97c3934ccaf99657b35
baaamk <brandon55321@gmail.com>
2025-11-03 03:26:31 +0900

    refactor(domain): 검증 및 순위 기능 추가

--- src/main/java/lotto/model/domain/WinningLotto.java
+++ src/main/java/lotto/model/domain/WinningLotto.java
@@ -1,14 +1,35 @@
 package lotto.model.domain;
 
+
+import lotto.exception.ErrorMessage;
+import lotto.model.domain.vo.BonusNumber;
+
 public class WinningLotto {
     private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
 
-    private WinningLotto(Lotto winningLotto) {
+    private WinningLotto(Lotto winningLotto, BonusNumber bonusNumber) {
         this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
     }
 
-    public static WinningLotto from(Lotto winningLotto) {
-        return new WinningLotto(winningLotto);
+    public static WinningLotto from(Lotto winningLotto, BonusNumber bonusNumber) {
+        validate(winningLotto, bonusNumber);
+        return new WinningLotto(winningLotto, bonusNumber);
     }
 
+    private static void validate(Lotto winningLotto, BonusNumber bonusNumber) {
+        if (bonusNumber.isMatchedWith(winningLotto)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
+    }
+
+    public Rank calculateRank(Lotto lotto) {
+        int matchCount = (int) lotto.numbers().stream()
+                .filter(winningLotto::contains)
+                .count();
+
+        boolean bonusMatched = bonusNumber.isMatchedWith(lotto);
+        return Rank.of(matchCount, bonusMatched);
+    }
 }
